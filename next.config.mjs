7ccc28e32fb8b0e6a82b86053058b6e913a25aa6
lin945 <linweixin945@gmail.com>
2026-07-23 11:23:32 +0800

    feat:升级next.js

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,12 @@
 const nextConfig = {
 	output: 'export',
 	images: {
-		domains: [
-			'blog-1304565468.cos.ap-shanghai.myqcloud.com',
-			'github.com',
-			'file-1304565468.cos.ap-nanjing.myqcloud.com'
+		unoptimized: true,
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'github.com'
+			}
 		]
 	}
 };
